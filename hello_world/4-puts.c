ad4802b0050442804dcc6941f684ef6291edd2c5
NachoCape <igcapezzolo@hotmail.com>
2022-06-06 07:18:00 -0700

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <string.h>
-int main()
+
+/**
+ * main - This C program use puts to print
+ * Return: devuelve 0
+ */
+int main(void)
 {
 	char res[] = "\"Programming is like building a multilingual puzzle";
+
+
 	puts(res);
-	return 0;
+
+	(return 0);
 }
