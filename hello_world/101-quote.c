c3193a9635e6bdddf6a6c73fecd426141f1aa871
NachoCape <igcapezzolo@hotmail.com>
2022-06-06 09:31:58 -0700

    task8

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - This C program is for print without printf or puts 
+ * main - This C program is for print without printf or puts
  * Return: devuelve 1
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 60 );	
-return(1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+return (1);
 }
