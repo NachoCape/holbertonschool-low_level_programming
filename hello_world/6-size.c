aad9465d479fba5c3e3da1d82b34840e7e27f720
NachoCape <igcapezzolo@hotmail.com>
2022-06-06 08:13:52 -0700

    task6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 /**
- * main - This C program use puts to print
+ * main - This C program prints the size of various types
  * Return: devuelve 0
  */
 
 int main(void)
 {
+	int i;
+	char c;
+	float f;
 	long int li;
 	long long int lli;
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of a int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(c));
+	printf("Size of a int: %zu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(lli));
-	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 return (0);
 }
