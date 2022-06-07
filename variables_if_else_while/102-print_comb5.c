5570fa99bcf04882ddd66d5d2c957d75def40742
NachoCape <igcapezzolo@hotmail.com>
2022-06-06 20:21:00 -0700

    task12

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -17,32 +17,23 @@
 
 int main(void)
 {
-	int i, j, k, l;
+	int i, j;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = 48; j <= 57; j++)
+		for (j = 0; j <= 99; j++)
 		{
-			for (k = i; k <= 57; k++)
+			if (i < j)
 			{
-				for (l = j + 1; l <= 57; l++)
-				{
-					if (i == 48 && j == 48 && k != 48)
-					{
-						l--;
-					}
-					putchar(i);
-					putchar(j);
-					putchar(' ');
-					putchar(k);
-					putchar(l);
-					if (i == 57 && j == 56 && k == 57 && l == 57)
-						continue;
-					putchar(',');
-					putchar(' ');
-					if (i == 48 && j == 48 && k != 48)
-						l++;
-				}
+				putchar(i);
+				putchar(j);
+				putchar(' ');
+				putchar(k);
+				putchar(l);
+				if (i == 98 && j == 99)
+					continue;
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
