9135bbc704c6ddb6485b163671702fbf831893b5
NachoCape <igcapezzolo@hotmail.com>
2022-06-09 05:22:39 -0700

    add readme and main.h

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: letter to be check
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	int res;
+
+	if (c >= 'A' && c <= 'Z')
+		res = 1;
+	else
+		res=0;
+}
