198c8c223be9d1f2d832a029aa1c6fd90806ffec
NachoCape <igcapezzolo@hotmail.com>
2022-06-09 15:38:12 -0700

    task12

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -7,7 +7,9 @@ void print_times_table(int n)
 {
 	int i, j, res;
 
-	if (n <= 15 || n >= 0)
+	if (n > 15 || n < 0)
+		_putchar('\n');
+	else
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -38,5 +40,4 @@ void print_times_table(int n)
 			}
 		}
 	}
-	_putchar('\n');
 }
