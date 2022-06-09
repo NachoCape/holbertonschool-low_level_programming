b097b27cf484cdb1910ebe534b14449350099bb2
NachoCape <igcapezzolo@hotmail.com>
2022-06-09 16:28:58 -0700

    task15

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - finds and prints the sum of the even-valued terms
+ *  followed by a new line.
+ *  Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long next_term, first, second, sum;
+
+	first = 1;
+	second = 2;
+	sum = 2;
+	next_term = first + second;
+	for (i = 3; i <= 50; i++)
+	{
+		first = second;
+		second = next_term;
+		next_term = first + second;
+		sum = sum + next_term;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
