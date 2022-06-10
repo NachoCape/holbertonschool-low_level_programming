8f32086c77f5696c2ed1991aafe29ba5666f0bdc
NachoCape <igcapezzolo@hotmail.com>
2022-06-10 06:45:26 -0700

    fix_task104

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -2,6 +2,33 @@
 #include <stdio.h>
 
 /**
+ * fibo - a
+ * @w: first half
+ * @x: first half part2
+ * @y: second half
+ * @z: second half part2
+ * @i: continue in i
+ */
+void fibo(long int w, long int x, long int y, long int z, int i)
+{
+	long int a, b;
+
+	while (i <= 98)
+	{
+		a = w + y;
+		b = x + z;
+		printf("%lu%lu", a, b);
+		if (i != 98)
+			printf(", ");
+		w = y;
+		x = z;
+		y = a;
+		z = b;
+		i++;
+	}
+}
+
+/**
  * main - prints the first 98 Fibonacci numbers, starting with
  * 1 and 2, followed by a new line.
  * The numbers should be separated by comma, followed by a space ,
@@ -16,21 +43,28 @@
 int main(void)
 {
 	int i;
-	unsigned long next_term, first, second;
+	long int w, x, y, z, a, b, first, sec, sig;
 
+	/** First 92 numbers of fibonacci */
 	first = 1;
-	second = 2;
-	next_term = first + second;
+	sec = 2;
+	sig = first + sec;
 	printf("%d, %d, ", 1, 2);
-	for (i = 3; i <= 98; i++)
+	for (i = 3; i <= 90; i++)
 	{
-		printf("%lu", next_term);
-		if (i != 98)
-			printf(", ");
-		first = second;
-		second = next_term;
-		next_term = first + second;
+		printf("%lu", sig);
+		printf(", ");
+		first = sec;
+		sec = sig;
+		sig = first + sec;
 	}
+	/** Divide the big numbers(last 6) */
+	w = first / 10000000000;
+	x = first % 10000000000;
+	y = sec / 10000000000;
+	z = sec % 10000000000;
+	/** Last 6 numbers of fibonacci */
+	fibo(w, x, y, z, i);
 	printf("\n");
 	return (0);
 }
