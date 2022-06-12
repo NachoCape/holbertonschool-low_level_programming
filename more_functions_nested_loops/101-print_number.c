2c508dcafafcbb85ac226019050734d9cb78db70
NachoCape <igcapezzolo@hotmail.com>
2022-06-11 17:35:33 -0700

    task12

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * expo - calculate how many times 10 wanna be multiplied by himself
+ * @count: numbers of times to multiplie 10
+ * Return: 10 ^ count
+ */
+
+int expo(int count)
+{
+	int res, k;
+
+	res = 1;
+	for (k = 1; k <= count; k++)
+		res = res * 10;
+	return (res);
+}
+
+/**
  * print_number - This function prints an integer
  * Description: You are not allowed to use long
  * You can only use _putchar function to print
@@ -11,22 +27,31 @@
 
 void print_number(int n)
 {
-	int aux, count, i;
+	int aux, aux2, count, digit;
 
-	aux = n;
-	count = 0;
-	while (aux % 10 != aux)
+	if (n == 0)
+		_putchar('0');
+	else
 	{
-		aux = aux / 10;
-		count++;
-	}
-	aux = n;
-	if (count > 0)
-		for (i = count; i >= 1; i--)
+		if (n < 0)
 		{
-			aux = aux / (10 * i);
-			_putchar(aux + '0');
-			aux = n;
+			n = n * -1;
+			_putchar('-');
 		}
-	_putchar(n % 10 + '0');
+		aux = n;
+		count = 0;
+		while (aux % 10 != aux)
+		{
+			aux = aux / 10;
+			count++;
+		}
+		while (count >= 0)
+		{
+			aux2 = expo(count);
+			count--;
+			digit = n / aux2;
+			_putchar(digit % 10 + '0');
+		}
+	}
+
 }
