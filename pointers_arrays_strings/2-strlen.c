c67734bcf901fc60d9acd06d3eb86ff3fcc83802
NachoCape <igcapezzolo@hotmail.com>
2022-06-14 08:24:00 -0700

    task2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - eturns the length of a string
+ * @s: a string of chars
+ * Return: The length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
