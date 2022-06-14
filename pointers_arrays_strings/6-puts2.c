9fcc90894e8c0c21d4b2cb9453a05c958dc779e6
NachoCape <igcapezzolo@hotmail.com>
2022-06-14 10:23:14 -0700

    task6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,6 +2,21 @@
 #include <stdio.h>
 
 /**
+ * _strlen - eturns the length of a string
+ * @s: a string of chars
+ * Return: The length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
  * puts2 - prints every other character of a string, starting
  * with the first character, followed by a new line.
  * @str: string to printf
@@ -10,8 +25,9 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int le = _strlen(str);
 
-	while (str[i] != '\0')
+	while (i <= le && str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i = i + 2;
