2de55b820e8f3029c2988abb51bdb8da98db3f8b
NachoCape <igcapezzolo@hotmail.com>
2022-06-15 18:26:16 -0700

    task2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,9 +9,10 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != c)
+	while (*s != '\0' && *s != c)
 		s++;
-	if (*s != c)
-		s = '\0';
-	return (s);
+	if (*s == c)
+		return (s);
+	else
+		return ('\0');
 }
