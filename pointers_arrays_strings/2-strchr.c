bb87b459eb22518e5d1e51507336b12e7a958cd2
NachoCape <igcapezzolo@hotmail.com>
2022-06-15 18:02:27 -0700

    task2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string of char
+ * @c: char
+ * Return: pointer to a char
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0' && *s != c)
+		s++;
+	return (s);
+}
