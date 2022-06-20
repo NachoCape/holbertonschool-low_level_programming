7cb138f92474eb7a32a83e705303ae6647fd7b47
NachoCape <igcapezzolo@hotmail.com>
2022-06-20 07:57:11 -0700

    fix_task2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,21 +1,28 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - that returns the length of a string
+ * count - recursive function to count the lenght of a string
+ * @res: integer (result)
  * @s: string of char
- * Return: the lenght of the string
+ * Return: the large of the string
  */
 
 int count(int res, char *s)
 {
 	if (*s == '\0')
-		return res;
+		return (res);
 	return (res = count(res + 1, s + 1));
 }
+
+/**
+ * _strlen_recursion - that returns the length of a string
+ * @s: string of char
+ * Return: the lenght of the string
+ */
 
 int _strlen_recursion(char *s)
 {
 	int res = 0;
-	
+
 	return (res = count(res, s));
 }
