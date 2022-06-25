0884e7793f3a37fc52631d9b7412f9323e93b420
NachoCape <igcapezzolo@hotmail.com>
2022-06-24 17:03:31 -0700

    task2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: unsigned int
+ * @size: unsigned int
+ * Return: a pointer to the allocated memory, If nmemb or size is 0, then
+ * _calloc returns NULL, If malloc fails, then _calloc returns NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *res = NULL;
+
+	if (nmemb != 0 && size != 0)
+		res = malloc(nmemb * size);
+	return (res);
+}
