ad5f00659970d6bcc14544b75f67bcfe299bd97d
NachoCape <igcapezzolo@hotmail.com>
2022-06-27 16:19:37 -0700

    task0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - information about the dog
+ * @name: string of char
+ * @age: float
+ * @owner: string of char
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
