3b434ba5b1b27364a28b0c8855e7be1d574bc23c
NachoCape <igcapezzolo@hotmail.com>
2022-06-28 11:47:48 -0700

    aux_functions_for_task3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - sum two numbers
+ * @a: integer
+ * @b: integer
+ * Return: the sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * up_sub - difference 2 numbers
+ * @a: integer
+ * @b: integer
+ * Return: the difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * up_mul - product two numbers
+ * @a: integer
+ * @b: integer
+ * Return: the product of a and b
+ */
+
+int op_mul(int a, int b);
+{
+	return (a * b);
+}
+
+/**
+ * up_div - div two numbers
+ * @a: integer
+ * @b: integer
+ * Return: the result of the division of a by b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of the division of two numbers
+ * @a: integer
+ * @b: integer
+ * Return: the remainder of the division of a by b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
