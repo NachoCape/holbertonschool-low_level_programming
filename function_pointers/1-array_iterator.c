5d4c0f3fa8037ad4f0d6bb545d20601fdca7b013
NachoCape <igcapezzolo@hotmail.com>
2022-06-28 11:23:49 -0700

    fix_betty_task1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator executes a function given as a
+ * array_iterator - executes a function given as a
  * parameter on each element of an array.
  * @array: array of integer
  * @size: size of the array of type size_t
