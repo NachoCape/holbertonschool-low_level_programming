bfb610fdab598dadbfef4ce50f94cb9a99460427
NachoCape <igcapezzolo@hotmail.com>
2022-06-27 17:22:53 -0700

    task4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,20 +15,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_d = NULL;
 
 	new_d = malloc(sizeof(dog_t));
-	if (new_d)
-		new_d->name = _strdup(name);
-	if (new_d->name == NULL)
+	if (new_d != NULL)
 	{
-		free(new_d);
-		return (NULL);
+			new_d->name = _strdup(name);
+		if (new_d->name == NULL)
+		{
+			free(new_d);
+			return (NULL);
+		}
+		new_d->owner = _strdup(owner);
+		if (new_d->owner == NULL)
+		{
+			free(new_d->name);
+			free(new_d);
+			return (NULL);
+		}
+		new_d->age = age;
 	}
-	new_d->owner = _strdup(owner);
-	if (new_d->owner == NULL)
-	{
-		free(new_d->name);
-		free(new_d);
-		return (NULL);
-	}
-	new_d->age = age;
 	return (new_d);
 }
