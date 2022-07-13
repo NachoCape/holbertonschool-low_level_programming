edc5a796578c9b0463126ab0529866704212d719
NachoCape <igcapezzolo@hotmail.com>
2022-07-13 07:47:04 -0700

    task9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 #include "2-add_nodeint.c"
-
+#include <stdio.h>
 /**
  * insert_nodeint_at_index - inserts a new node at a given position
  * @head: pointer to a pointer struct
@@ -17,12 +17,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (!p && idx == 0)
 		return (add_nodeint(head, n));
-	while (p && i < idx - 1)
+	while (p && idx != 0 && i < idx - 1)
 	{
 		p = p->next;
 		i++;
 	}
-	if (!p && i != idx - 1)
+	if (!p)
 		return (NULL);
 	if (idx == 0)
 		nodo = add_nodeint(head, n);
