a0b7f6dee69f73175eea35aa43a1afae6a83d126
NachoCape <igcapezzolo@hotmail.com>
2022-07-12 19:49:19 -0700

    task5

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *aux = *head;
 
-	while (aux)
-	{
-		aux = (*head)->next;
-		free(*head);
-		(*head) = aux;
-	}
+	if (head)
+		while (aux)
+		{
+			aux = (*head)->next;
+			free(*head);
+			(*head) = aux;
+		}
 }
