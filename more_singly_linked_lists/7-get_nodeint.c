75c9a1f6cf903df3fe43461adea0605fb0dbe54d
NachoCape <igcapezzolo@hotmail.com>
2022-07-13 05:30:00 -0700

    task7

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -3,11 +3,12 @@
 /**
  * get_nodeint_at_index - returns the nth node of a listint_t linked list
  * @head: pointer to a pointer struct
+ * @index: is the index of the node, starting at 0
  *
  * Description: where index is the index of the node, starting at 0
  * if the node does not exist, return NULL
  *
- * return: returns the nth node of a listint_t linked list
+ * Return: returns the nth node of a listint_t linked list
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
