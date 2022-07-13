8b479f3d568a32ca5590ad7aa8fed646f36d48f9
NachoCape <igcapezzolo@hotmail.com>
2022-07-12 19:35:58 -0700

    task5

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to head
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *aux = *head;
+
+	while (*head)
+	{
+		*head = *head->next;
+		free(aux);
+		aux = *head;
+	}
+}
