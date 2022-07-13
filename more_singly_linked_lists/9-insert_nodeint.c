873aae9f45edfbe607dda4aede8e23ec7133ae8d
NachoCape <igcapezzolo@hotmail.com>
2022-07-13 07:12:13 -0700

    task9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	if (!p && idx == 0)
-		nodo = add_nodeint(head, n);
+		return (add_nodeint(head, n));
 	while (p && i < idx - 1)
 	{
 		p = p->next;
