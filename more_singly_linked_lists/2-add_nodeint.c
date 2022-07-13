7681595f446c2cd939e22d3c82ca76d6932f25a9
NachoCape <igcapezzolo@hotmail.com>
2022-07-12 19:10:18 -0700

    task2

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	node = malloc(sizeof(listint_t));
 	if (node)
 	{
-		node->int = n;
+		node->n = n;
 		if (*head)
 			node->next = *head;
 		else
