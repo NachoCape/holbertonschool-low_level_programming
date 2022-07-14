cacc57f739c56c3aed281852c554f02db284ac7c
NachoCape <igcapezzolo@hotmail.com>
2022-07-14 07:58:51 -0700

    task5

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,7 +1,13 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * rata - ratita
+ * Return: void
+ */
+
 void __attribute__((constructor)) rata()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
