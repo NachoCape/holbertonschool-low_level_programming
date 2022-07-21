8132ad6b147357e5873b2cb3f3383904468a74c5
NachoCape <igcapezzolo@hotmail.com>
2022-07-21 10:47:42 -0700

    task7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			node = add_dnodeint_end(h, n);
 		else
 		{
-			while (idx != i - 1)
+			while (i != idx - 1)
 			{
 				i++;
 				aux = aux->next;
