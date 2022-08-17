e74e9c97556f5664c39da68c6260768520b3a3f4
NachoCape <igcapezzolo@hotmail.com>
2022-08-17 16:59:32 -0700

    fix ht is NULL task3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -19,6 +19,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node = NULL;
 	char *new_key = strdup(key), *new_value = strdup(value);
 
+	if (!ht)
+		return (0);
 	node = malloc(sizeof(hash_node_t));
 	if (!node || !new_key || !new_value)
 	{
