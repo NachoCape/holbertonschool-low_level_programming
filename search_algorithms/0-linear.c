8e08a19d67075e5b265ae2e9299bc439dace4d5e
NachoCape <igcapezzolo@hotmail.com>
2022-08-25 09:55:49 -0700

    border case fix at task0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -14,6 +14,8 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
+	if (!array)
+		return (-1);
 	while (i < size && array[i] != value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
